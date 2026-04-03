17c41440678ccf3645a0008d8653290cac3a02d6
ptmcg <ptmcg@austin.rr.com>
2026-04-02 23:53:04 -0500

    Update multi_directional_dict.py example; add some female characters; make lookup statements more explicit; add some exception cases (lookup for non-existent department, add duplicate and null value characters)

--- examples/multi_directional_dict.py
+++ examples/multi_directional_dict.py
@@ -6,19 +6,20 @@
 
 import littletable as lt
 
+# Data for a company employee roster
 employees = lt.csv_import("""\
 name,employee_id,tax_id,primary_phone,primary_dept
 Wile E Coyote,A1234,123-45-6789,(555)111-2222,R&D
-Hong Kong Phooey,B5678,234-56-7890,(555)222-3333,Sales
+Daphne Blake,B5678,234-56-7890,(555)222-3333,Sales
 Magilla Gorilla,C9012,345-67-8901,(555)333-4444,Operations
 Quick Draw McGraw,D3456,456-78-9012,(555)444-5555,Finance
 Snagglepuss,A7890,567-89-0123,(555)555-6666,Sales
-Jabberjaw,B1357,678-90-1234,(555)666-7777,R&D
+Velma Dinkley,B1357,678-90-1234,(555)666-7777,R&D
 Secret Squirrel,C2468,789-01-2345,(555)777-8888,Operations
 Morocco Mole,D9753,890-12-3456,(555)888-9999,Finance
 Atom Ant,A1111,901-23-4567,(555)999-0000,Sales
-Squiddly Diddly,B2222,012-34-5678,(555)000-1111,R&D
-Grape Ape,C3333,112-23-3445,(555)121-2323,Operations
+Dick Dastardly,B2222,012-34-5678,(555)000-1111,R&D
+Penelope Pitstop,C3333,112-23-3445,(555)121-2323,Operations
 Deputy Dawg,D4444,223-34-4556,(555)232-3434,Finance
 """
 )
@@ -33,7 +34,7 @@ employees.create_index("employee_id", unique=True)
 employees.create_index("tax_id", unique=True)
 employees.create_index("primary_phone", unique=True)
 
-# Accessing the table by non-unique key behaves like a defaultdict(Table)
+# Accessing the table by non-unique key behaves like a `defaultdict(Table)`
 # - if the key is present, a new Table is returned containing the matching records
 #   (with the same indexes defined as in the original table)
 # - if the key is absent, an empty Table is returned
@@ -45,14 +46,50 @@ employees.present()
 # List the R&D department
 employees.by.primary_dept["R&D"]("R&D").present()
 
+# List the Legal department (which has no employees)
+employees.by.primary_dept["Legal"]("Legal").present()
+
 print("\nLookup by name")
 for name in "Snagglepuss/Quick Draw McGraw/Secret Squirrel".split("/"):
-    print(name, employees.by.name[name].primary_phone, employees.by.name[name].employee_id)
+    employee = employees.by.name[name]
+    print(name, employee.primary_phone, employee.employee_id)
 
 print("\nLookup by primary phone number")
 for phone_num in "(555)555-6666 (555)444-5555 (555)777-8888".split():
-    print(phone_num, employees.by.primary_phone[phone_num].name, employees.by.primary_phone[phone_num].employee_id)
+    employee = employees.by.primary_phone[phone_num]
+    print(phone_num, employee.name, employee.employee_id)
 
 print("\nLookup by employee id")
 for emp_id in "A7890 D3456 C2468".split():
-    print(emp_id, employees.by.employee_id[emp_id].name, employees.by.employee_id[emp_id].primary_phone)
+    employee = employees.by.employee_id[emp_id]
+    print(emp_id, employee.name, employee.primary_phone)
+
+# Unique indexes raise exception when inserting a duplicate
+print("\nTry inserting a record with a duplicate name")
+try:
+    employees.insert(
+        {
+        "name": "Dick Dastardly",
+        "employee_id": "B2222",
+        "tax_id": "012-34-5678",
+        "primary_phone": "(555)000-1111",
+        "primary_dept": "R & D",
+        }
+    )
+except Exception as e:
+    print(f"{type(e).__name__}: {e}")
+
+# By default, unique indexes raise exception when inserting an entry with a null key
+print("\nTry inserting a record with a null name")
+try:
+    employees.insert(
+        {
+        "name": None,
+        "employee_id": "B2222",
+        "tax_id": "012-34-5678",
+        "primary_phone": "(555)000-1111",
+        "primary_dept": "R & D",
+        }
+    )
+except Exception as e:
+    print(f"{type(e).__name__}: {e}")
